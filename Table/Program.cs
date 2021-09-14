548192f6b81ef83904e81866dc883c7fdec72124
Dell <Dell@ZISHAN>
2021-09-14 18:26:06 +0530

    Add project files.

---
+++ Table/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Table
+{
+    class Table
+    {
+        //method declaration
+        public void table(int num)
+        {
+            int i, t, j;
+            for (j = 1; j <= 10; j++)
+            {
+                for (i = num; i <= num + 3; i++)  
+                {
+                    t = i * j;
+                    Console.Write("{0}*{1}={2}\t", i, j, t);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table obj = new Table();
+            Console.WriteLine("Enter The Number");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            obj.table(n);
+        }
+    }
+}
